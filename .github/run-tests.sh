37d3643e705afe9da02211877eda6ec4c8b72b99
Steve Kemp <steve@steve.org.uk>
2019-09-17 21:12:37 +0300

    Added stub CI support.

---
+++ .github/run-tests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This will allow the linter to be installed.  All a mess.
+rm go.mod
+
+# Install tools to test our code-quality.
+go get -u golang.org/x/lint/golint
+go get -u golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
+go get -u honnef.co/go/tools/cmd/staticcheck
+
+# Init the modules
+go mod init
+
+# Run the static-check tool - we ignore errors in goserver/static.go
+t=$(mktemp)
+staticcheck -checks all ./... > $t
+if [ -s $t ]; then
+    echo "Found errors via 'staticcheck'"
+    cat $t
+    rm $t
+    exit 1
+fi
+rm $t
+
+# At this point failures cause aborts
+set -e
+
+# Run the linter
+echo "Launching linter .."
+golint -set_exit_status ./...
+echo "Completed linter .."
+
+# Run the shadow-checker
+echo "Launching shadowed-variable check .."
+go vet -vettool=$(which shadow) ./...
+echo "Completed shadowed-variable check .."
+
+# Run golang tests
+go test ./...
