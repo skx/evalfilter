eac96b133c49ecefb2743716c3f537e32f6a96ae
Steve Kemp <steve@steve.org.uk>
2019-09-24 22:01:46 +0300

    Added fuzzing support/documentation

---
+++ parser/fuzz.go
@@ -0,0 +1,22 @@
+// +build gofuzz
+
+//
+// This file is only used for fuzzing the parser,
+// which will detect hangs, infinite loops & etc.
+//
+
+package parser
+
+import "github.com/skx/evalfilter/lexer"
+
+// Fuzz is the function that our fuzzer-application uses.
+// See `FUZZING.md` in our distribution for how to invoke it.
+func Fuzz(data []byte) int {
+
+	l := lexer.New(string(data))
+	p := New(l)
+
+	p.ParseProgram()
+	return 1
+
+}
