96a09f707859e4a98237a03558b72926fba5b05f
Steve Kemp <steve@steve.org.uk>
2019-11-06 17:13:39 +0200

    Improve clarity.
    * Added many more comments to our opcodes.
    * Removed the argument-reading.
      * Replace it instead with code to calculate lengths.

--- code/code.go
+++ code/code.go
@@ -1,98 +1,158 @@
-// Package code contains definitions of the bytecode instruction.
-//
-// The instructions are used in two different ways, first of all the
-// compiler will generate them as it walks the AST which resulted from
-// parsing the users' program.  Secondly the virtual machine itself
-// will intepret those instructions.
+// Package code contains definitions of the bytecode instructions
+// our compiler emits, and our virtual machine executes.
 package code
-
-import "encoding/binary"
-
-// Instructions is a type alias.
-type Instructions []byte
 
 // Opcode is a type-alias.
 type Opcode byte
 
+// Instructions is a type alias.
+type Instructions []byte
+
 // Opcodes we support
 const (
 
-	// load a constant object; number, string, bool.
-	// 16-bit argument is the offset of the constant
+	// Push the value of one of our constant objects onto the stack.
+	//
+	// The 16-bit argument is the offset of the constant.
 	OpConstant Opcode = iota
 
-	// Jump for IF implementation
-	// 16-bit argument is the offset to jump to
+	// Unconditionally jump to the specified offset.
+	//
+	// 16-bit argument is the offset to jump to.
 	OpJump
+
+	// Pop a value from the stack, and if the value is false then jump
+	// to the specified offset.
+	//
+	// 16-bit argument is the offset to jump to.
 	OpJumpIfFalse
 
-	// Function call.
-	// 16-bit offset is the number of arguments used
+	// Call one of our built-in functions.
+	//
+	// Pop the name from the the stack, then use the 16-bit argument
+	// as the number of additional items to pop off the stack.  (i.e
+	// the number of arguments to pass to the function.)
+	//
+	// Once complete push the result of the call back to the stack.
 	OpCall
 
-	// Load a variable by name
+	// Load a variable by name.
 	// 16-bit offset to the name to lookup
+	//
+	// TODO: This could be a single-byte operation, we could
+	// pop the name from the stack ..
 	OpLookup
 
 	//
-	// Everything higher than this opcode has no arguments.
+	// NOTE:  This is a fake opcode.
 	//
-	// This is a fake opcode.
+	// Every opcode below this one take zero arguments.
 	//
 	OpCodeSingleArg
 
 	// Set a variable by name
 	OpSet
 
-	// Load true/false value.
+	// Push a TRUE value onto the stack.
 	OpTrue
+
+	// Push a FALSE value onto the stack.
 	OpFalse
 
-	// Maths operations
+	// Pop two values from the stack, add them, and push the result.
 	OpAdd
+
+	// Pop two values from the stack, subtract them, and push the result.
 	OpSub
+
+	// Pop two values from the stack, multiply them, and push the result.
 	OpMul
+
+	// Pop two values from the stack, divide them, and push the result.
 	OpDiv
+
+	// Pop two values from the stack, run a modulus op, push the result.
 	OpMod
+
+	// Pop two values from the stack, raise to the power, push the result.
 	OpPower
 
-	// Return from script.
+	// Use  the top value from the stack as a the return value
+	// and cease exeution.
 	OpReturn
 
-	// Prefix operations.
+	// Pop a value from the the stack, invert, push back.
 	OpMinus
+
+	// Pop a value from the the stack, negate, push back.
 	OpBang
+
+	// Pop a value from the stack, and push back the square root of it.
 	OpRoot
 
-	// Comparison operations
+	// Pop two values and push TRUE if the first is less than the second,
+	// otherwise push FALSE.
 	OpLess
+
+	// Pop two values and push TRUE if the first is less than, or equal
+	// to the second, otherwise push FALSE.
 	OpLessEqual
+
+	// Pop two values and push TRUE if the first is greater than the second,
+	// otherwise push FALSE
 	OpGreater
+
+	// Pop two values and push TRUE if the first is greater than, or equal
+	// to the second.  Otherwise push TRUE.
 	OpGreaterEqual
 
-	// Equality
+	// Pop two values from the stack.  If equal push TRUE, else push FALSE.
 	OpEqual
+
+	// Pop two values from the stack.  If unequal push TRUE, else push FALSE.
 	OpNotEqual
 
-	// Regexp (string) match
+	// Pop two values from the stack, if the first matches the regexp
+	// in the second push TRUE, else push FALSE.
 	OpMatches
+
+	// Pop two values from the stack, if the first does not match the
+	// regexp in the second push TRUE, else push FALSE.
 	OpNotMatches
 
-	// Logical
+	// Pop two values from the stack.  If both are TRUE push TRUE,
+	// otherwise push FALSE.
 	OpAnd
+
+	// Pop two values from the stack.  If either is TRUE push TRUE,
+	// otherwise push FALSE.
 	OpOr
 
-	// Last opcode - NOP
+	//
+	// NOTE:  This is a fake opcode.
+	//
+	// This is our final opcode.
+	//
 	OpFinal
 )
 
-// ReadUint16 Return a 16-bit number from the stream.
-func ReadUint16(ins Instructions) uint16 {
-	return binary.BigEndian.Uint16(ins)
+// Length returns the length of the given opcode.
+//
+// All opcodes are a single byte, but some require a mandatory argument.
+//
+// This function returns the total expected length of the opcode and
+// any required argument.  Note that at the moment all opcodes require
+// either zero or one arguments (where the argument is a two-byte
+// 16-bit integer).  This might change in the future.
+func Length(op Opcode) int {
+	if op < OpCodeSingleArg {
+		return 3
+	}
+	return 1
 }
 
-// String converts the given opcode to a string.
-// This is useful for diagnostics.
+// String converts the given opcode to a string, this is used by our
+// bytecode disassembler/dumper.
 func String(op Opcode) string {
 
 	switch op {
