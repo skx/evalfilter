e60ae1cf2b3c5e8855a0549a3aabe4f0c5720d7a
Steve Kemp <steve@steve.org.uk>
2019-11-07 20:09:35 +0200

    Also increase code-coverage here :)

--- code/code_test.go
+++ code/code_test.go
@@ -10,10 +10,24 @@ func TestOpcodes(t *testing.T) {
 	var i Opcode
 
 	for i <= OpFinal {
+
+        // Stringify and check it looks sane
 		x := String(i)
 		if !strings.HasPrefix(x, "Op") {
 			t.Fatalf("opcode doesn't have a good prefix:%s", x)
 		}
+
+        // Opcode length
+        if ( i < OpCodeSingleArg ) {
+            if ( 3 != Length(i) ) {
+                t.Fatalf("Invalid length of opcode %s", x)
+            }
+        } else {
+            if ( 1 != Length(i) ) {
+                t.Fatalf("Invalid length of opcode %s", x)
+            }
+
+        }
 
 		i++
 	}
