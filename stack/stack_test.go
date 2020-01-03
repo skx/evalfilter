f395a1dd0c68ddb8bbf4a6d3852eda62d32022a1
Steve Kemp <steve@steve.org.uk>
2020-01-03 17:36:48 +0200

    Bump test-coverage of stack-package to 100%

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -29,6 +29,14 @@ func TestStack(t *testing.T) {
 		t.Errorf("stack has a size-mismatch")
 	}
 
+	out := s.Export()
+	if len(out) != 1 {
+		t.Errorf("Exported stack has wrong size")
+	}
+	if out[0] != "Steve Kemp" {
+		t.Errorf("exported stack has the wrong value")
+	}
+
 	val, err := s.Pop()
 
 	if err != nil {
@@ -39,6 +47,10 @@ func TestStack(t *testing.T) {
 	}
 	if s.Size() != 0 {
 		t.Errorf("stack has a size-mismatch")
+	}
+	out = s.Export()
+	if len(out) != 0 {
+		t.Errorf("Exported stack has wrong size")
 	}
 
 	if val.Inspect() != "Steve Kemp" {
@@ -51,12 +63,27 @@ func TestStackOrder(t *testing.T) {
 	s := New()
 
 	s.Push(&object.String{Value: "Steve Kemp"})
-	s.Push(&object.String{Value: "Adam Ant"})
+	s.Push(&object.String{Value: "Adam Ant\n"})
 	if s.Empty() {
 		t.Errorf("Stack should not be empty after adding item.")
 	}
 	if s.Size() != 2 {
 		t.Errorf("stack has a size-mismatch")
+	}
+
+	// export to test
+	out := s.Export()
+	if len(out) != 2 {
+		t.Errorf("Exported stack has wrong size")
+	}
+
+	// Test that the string is unescaped
+	// i.e. "\" "n", rather than "\n".
+	if out[0] != "Steve Kemp" {
+		t.Errorf("exported stack has the wrong value")
+	}
+	if out[1] != "Adam Ant\\n" {
+		t.Errorf("exported stack has the wrong value")
 	}
 
 	val, err := s.Pop()
@@ -67,7 +94,7 @@ func TestStackOrder(t *testing.T) {
 		t.Errorf("stack has a size-mismatch")
 	}
 
-	if val.Inspect() != "Adam Ant" {
+	if val.Inspect() != "Adam Ant\n" {
 		t.Errorf("Stack push/pop mismatch:%s", val.Inspect())
 	}
 
