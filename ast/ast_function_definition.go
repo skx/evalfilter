5b69c362e897113d6af6a357989f253731fe7986
Steve Kemp <steve@steve.org.uk>
2020-08-25 20:48:12 +0300

    Minor reformatting of stringified version

--- ast/ast_function_definition.go
+++ ast/ast_function_definition.go
@@ -8,8 +8,13 @@ import (
 )
 
 // FunctionDefinition holds the definition of a user-defined function.
+//
+// This means a function which is defined within our scripting-language,
+// rather than a function implemented in Golang and exported to the
+// scripting environmnet.
 type FunctionDefinition struct {
-	// Token holds the token
+
+	// Token holds the name of the function.
 	Token token.Token
 
 	// Paremeters holds the function parameters.
@@ -33,6 +38,7 @@ func (fd *FunctionDefinition) String() string {
 	for _, p := range fd.Parameters {
 		params = append(params, p.String())
 	}
+	out.WriteString("function ")
 	out.WriteString(fd.TokenLiteral())
 	out.WriteString("(")
 	out.WriteString(strings.Join(params, ", "))
