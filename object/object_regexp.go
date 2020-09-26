77ce59a31ea5d84e87147824eb5c4c09a77bebf7
Steve Kemp <steve@steve.org.uk>
2020-09-26 11:38:42 +0300

    Define a regular-expression object.
    This was needed in the past, but we faked it with a string.

---
+++ object/object_regexp.go
@@ -0,0 +1,35 @@
+package object
+
+// Regexp wraps string and implements the Object interface.
+type Regexp struct {
+	// Value holds the string value this object wraps.
+	Value string
+
+	// Offset holds our iteration-offset
+	offset int
+}
+
+// Type returns the type of this object.
+func (r *Regexp) Type() Type {
+	return REGEXP
+}
+
+// Inspect returns a string-representation of the given object.
+func (r *Regexp) Inspect() string {
+	return r.Value
+}
+
+// True returns whether this object wraps a true-like value.
+//
+// Used when this object is the conditional in a comparison, etc.
+func (r *Regexp) True() bool {
+	return (r.Value != "")
+}
+
+// ToInterface converts this object to a go-interface, which will allow
+// it to be used naturally in our sprintf/printf primitives.
+//
+// It might also be helpful for embedded users.
+func (r *Regexp) ToInterface() interface{} {
+	return r.Value
+}
