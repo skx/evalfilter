caadddd660a6343dde8296078de363fad4c58bfb
Steve Kemp <steve@steve.org.uk>
2020-09-26 06:58:42 +0300

    The lexer tracks line/column.
    The lexer tracks lines/columns now, and ensures that the tokens which
    it returns have that information in them.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -30,32 +30,21 @@ type Lexer struct {
 
 	// Previous token.
 	prevToken token.Token
+
+	// Line contains our current line-number
+	line int
+
+	// column contains the place within the line where we are.
+	column int
 }
 
 // New creates a Lexer instance from the given string
 func New(input string) *Lexer {
-	l := &Lexer{characters: []rune(input)}
+
+	// Line counting starts at one.
+	l := &Lexer{characters: []rune(input), line: 1}
 	l.readChar()
 	return l
-}
-
-// GetLine returns the rough line-number of our current position.
-//
-// This is used to report errors in a more humane manner.
-func (l *Lexer) GetLine() int {
-	line := 0
-	chars := len(l.characters)
-	i := 0
-
-	for i < l.readPosition && i < chars {
-
-		if l.characters[i] == rune('\n') {
-			line++
-		}
-
-		i++
-	}
-	return line
 }
 
 // read forward one character.
@@ -67,6 +56,14 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
+
+	// Line counting
+	if l.ch == rune('\n') {
+		l.column = 0
+		l.line++
+	} else {
+		l.column++
+	}
 }
 
 // NextToken reads and returns the next token, skipping any intervening
@@ -87,87 +84,87 @@ func (l *Lexer) NextToken() token.Token {
 		if l.peekChar() == rune('&') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.AND, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.AND, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		}
 	case rune('|'):
 		if l.peekChar() == rune('|') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.OR, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.OR, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		}
 
 	case rune('='):
 		if l.peekChar() == rune('=') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else {
-			tok = newToken(token.ASSIGN, l.ch)
+			tok = l.newToken(token.ASSIGN, l.ch)
 		}
 
 	case rune(';'):
-		tok = newToken(token.SEMICOLON, l.ch)
+		tok = l.newToken(token.SEMICOLON, l.ch)
 
 	case rune('('):
-		tok = newToken(token.LPAREN, l.ch)
+		tok = l.newToken(token.LPAREN, l.ch)
 
 	case rune(')'):
-		tok = newToken(token.RPAREN, l.ch)
+		tok = l.newToken(token.RPAREN, l.ch)
 
 	case rune(','):
-		tok = newToken(token.COMMA, l.ch)
+		tok = l.newToken(token.COMMA, l.ch)
 
 	case rune('.'):
 		if l.peekChar() == rune('.') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.DOTDOT, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.DOTDOT, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else {
-			tok = newToken(token.PERIOD, l.ch)
+			tok = l.newToken(token.PERIOD, l.ch)
 		}
 
 	case rune('+'):
 		if l.peekChar() == rune('+') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.PLUSPLUS, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.PLUSPLUS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else if l.peekChar() == rune('=') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.PLUSEQUALS, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.PLUSEQUALS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else {
-			tok = newToken(token.PLUS, l.ch)
+			tok = l.newToken(token.PLUS, l.ch)
 		}
 
 	case rune('%'):
-		tok = newToken(token.MOD, l.ch)
+		tok = l.newToken(token.MOD, l.ch)
 
 	case rune('√'):
-		tok = newToken(token.SQRT, l.ch)
+		tok = l.newToken(token.SQRT, l.ch)
 
 	case rune('{'):
-		tok = newToken(token.LBRACE, l.ch)
+		tok = l.newToken(token.LBRACE, l.ch)
 
 	case rune('}'):
-		tok = newToken(token.RBRACE, l.ch)
+		tok = l.newToken(token.RBRACE, l.ch)
 
 	case rune('['):
-		tok = newToken(token.LSQUARE, l.ch)
+		tok = l.newToken(token.LSQUARE, l.ch)
 
 	case rune(']'):
-		tok = newToken(token.RSQUARE, l.ch)
+		tok = l.newToken(token.RSQUARE, l.ch)
 
 	case rune('-'):
 		if l.peekChar() == rune('-') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.MINUSMINUS, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.MINUSMINUS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else if l.peekChar() == rune('=') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.MINUSEQUALS, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.MINUSEQUALS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else {
-			tok = newToken(token.MINUS, l.ch)
+			tok = l.newToken(token.MINUS, l.ch)
 		}
 
 	case rune('/'):
@@ -191,18 +188,23 @@ func (l *Lexer) NextToken() token.Token {
 			if l.peekChar() == rune('=') {
 				ch := l.ch
 				l.readChar()
-				tok = token.Token{Type: token.SLASHEQUALS, Literal: string(ch) + string(l.ch)}
+				tok = token.Token{Type: token.SLASHEQUALS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 			} else {
-				tok = newToken(token.SLASH, l.ch)
+				tok = l.newToken(token.SLASH, l.ch)
 			}
 		} else {
 			str, err := l.readRegexp()
 			if err == nil {
-				tok.Type = token.REGEXP
+				tok.Column = l.column
+				tok.Line = l.line
 				tok.Literal = str
+				tok.Type = token.REGEXP
+
 			} else {
-				tok.Type = token.ILLEGAL
+				tok.Column = l.column
+				tok.Line = l.line
 				tok.Literal = err.Error()
+				tok.Type = token.ILLEGAL
 			}
 			return tok
 		}
@@ -210,79 +212,87 @@ func (l *Lexer) NextToken() token.Token {
 		if l.peekChar() == rune('*') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.POW, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.POW, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else if l.peekChar() == rune('=') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.ASTERISKEQUALS, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.ASTERISKEQUALS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else {
-			tok = newToken(token.ASTERISK, l.ch)
+			tok = l.newToken(token.ASTERISK, l.ch)
 		}
 
 	case rune('?'):
-		tok = newToken(token.QUESTION, l.ch)
+		tok = l.newToken(token.QUESTION, l.ch)
 	case rune(':'):
-		tok = newToken(token.COLON, l.ch)
+		tok = l.newToken(token.COLON, l.ch)
 
 	case rune('<'):
 		if l.peekChar() == rune('=') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.LTEQUALS, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.LTEQUALS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else {
-			tok = newToken(token.LT, l.ch)
+			tok = l.newToken(token.LT, l.ch)
 		}
 
 	case rune('>'):
 		if l.peekChar() == rune('=') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.GTEQUALS, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.GTEQUALS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else {
-			tok = newToken(token.GT, l.ch)
+			tok = l.newToken(token.GT, l.ch)
 		}
 
 	case rune('~'):
 		if l.peekChar() == rune('=') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.CONTAINS, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.CONTAINS, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		}
 
 	case rune('!'):
 		if l.peekChar() == rune('=') {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.NOTEQ, Literal: string(ch) + string(l.ch)}
+			tok = token.Token{Type: token.NOTEQ, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 		} else {
 			if l.peekChar() == rune('~') {
 				ch := l.ch
 				l.readChar()
-				tok = token.Token{Type: token.MISSING, Literal: string(ch) + string(l.ch)}
+				tok = token.Token{Type: token.MISSING, Literal: string(ch) + string(l.ch), Line: l.line, Column: l.column}
 			} else {
-				tok = newToken(token.BANG, l.ch)
+				tok = l.newToken(token.BANG, l.ch)
 			}
 		}
 
 	case rune('"'):
 		str, err := l.readString('"')
 		if err == nil {
-			tok.Type = token.STRING
+			tok.Column = l.column
+			tok.Line = l.line
 			tok.Literal = str
+			tok.Type = token.STRING
 		} else {
-			tok.Type = token.ILLEGAL
+			tok.Column = l.column
+			tok.Line = l.line
 			tok.Literal = err.Error()
+			tok.Type = token.ILLEGAL
 		}
 
 	case rune('\''):
 		str, err := l.readString('\'')
 
 		if err == nil {
-			tok.Type = token.STRING
+			tok.Column = l.column
+			tok.Line = l.line
 			tok.Literal = str
+			tok.Type = token.STRING
 		} else {
-			tok.Type = token.ILLEGAL
+			tok.Column = l.column
+			tok.Line = l.line
 			tok.Literal = err.Error()
+			tok.Type = token.ILLEGAL
 		}
 
 	case rune(0):
@@ -318,8 +328,8 @@ func (l *Lexer) NextToken() token.Token {
 }
 
 // return new token
-func newToken(tokenType token.Type, ch rune) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
+func (l *Lexer) newToken(tokenType token.Type, ch rune) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch), Line: l.line, Column: l.column}
 }
 
 // readIdentifier is designed to read an identifier (name of variable,
